49e1559e682a68c279ae4ffb9d1e7bbf900770b9
nihachandaluri <niharikachn2003@gmail.com>
2023-05-31 23:34:33 +0530

    final changes

--- script.js
+++ script.js
@@ -34,31 +34,31 @@ getButton.addEventListener("click", e =>{
 
 const exchangeIcon = document.querySelector("form .icon");
 exchangeIcon.addEventListener("click", ()=>{
-    let tempCode = fromCurrency.value; // temporary currency code of FROM drop list
-    fromCurrency.value = toCurrency.value; // passing TO currency code to FROM currency code
-    toCurrency.value = tempCode; // passing temporary currency code to TO currency code
-    loadFlag(fromCurrency); // calling loadFlag with passing select element (fromCurrency) of FROM
-    loadFlag(toCurrency); // calling loadFlag with passing select element (toCurrency) of TO
-    getExchangeRate(); // calling getExchangeRate
+    let tempCode = fromCurrency.value;
+    fromCurrency.value = toCurrency.value;
+    toCurrency.value = tempCode;
+    loadFlag(fromCurrency);
+    loadFlag(toCurrency); 
+    getExchangeRate(); 
 })
 
 function getExchangeRate(){
     const amount = document.querySelector("form input");
     const exchangeRateTxt = document.querySelector("form .exchange-rate");
     let amountVal = amount.value;
-    // if user don't enter any value or enter 0 then we'll put 1 value by default in the input field
+
     if(amountVal == "" || amountVal == "0"){
         amount.value = "1";
         amountVal = 1;
     }
     exchangeRateTxt.innerText = "Getting exchange rate...";
     let url = `https://v6.exchangerate-api.com/v6/2237769fe06f593e95efb2ca/latest/${fromCurrency.value}`;
-    // fetching api response and returning it with parsing into js obj and in another then method receiving that obj
+    
     fetch(url).then(response => response.json()).then(result =>{
-        let exchangeRate = result.conversion_rates[toCurrency.value]; // getting user selected TO currency rate
-        let totalExRate = (amountVal * exchangeRate).toFixed(2); // multiplying user entered value with selected TO currency rate
+        let exchangeRate = result.conversion_rates[toCurrency.value]; 
+        let totalExRate = (amountVal * exchangeRate).toFixed(2); 
         exchangeRateTxt.innerText = `${amountVal} ${fromCurrency.value} = ${totalExRate} ${toCurrency.value}`;
-    }).catch(() =>{ // if user is offline or any other error occured while fetching data then catch function will run
+    }).catch(() =>{ 
         exchangeRateTxt.innerText = "Something went wrong";
     });
 }
